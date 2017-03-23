72113ed4d7a5bf717ab265768c95b8afc1008a1c
Pietro Gori <pietro.gori@sequenza.com.br>
2017-03-23 00:21:19 -0300

    configurando rotas e partials views

--- public/js/directives/minhas-diretivas.js
+++ public/js/directives/minhas-diretivas.js
@@ -12,4 +12,20 @@ angular.module('minhasDiretivas', []).directive('meuPainel', function(){
 	ddo.templateUrl = 'js/directives/meu-painel.html';
 
 	return ddo;
+}).directive('minhaFoto', function(){
+
+	var ddo = {};
+
+	ddo.restrict = "AE";
+
+	ddo.transclude = true;
+
+	ddo.scope = { 
+		titulo: '@',
+		url: '@'
+	}
+
+	ddo.template = '<img class="img-responsive center-block" ng-src="{{url}}" alt="{{titulo}}">'
+
+	return ddo;
 });
