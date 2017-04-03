0e5561140c634d9861218881ea5c092df653ed98
Pietro Gori <pietro.gori@sequenza.com.br>
2017-04-03 01:08:30 -0300

    Aula 8 - Alteração e remoção

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,23 +1,45 @@
 angular.module('alurapic')
-.controller('FotoController', function($scope, $http){
+.controller('FotoController', function($scope, $http, $routeParams){
 
 	$scope.foto = {};
 	$scope.mensagem = '';
-
 	
+	if($routeParams.fotoId){
+		$http.get('/v1/fotos/' + $routeParams.fotoId)
+            .success(function(foto) {
+                $scope.foto = foto;
+            })
+            .error(function(erro) {
+                console.log(erro);
+                $scope.mensagem = 'Não foi possível obter a foto'
+            });
+	}
+
+
 	$scope.submeter = function(){
 		if($scope.formulario.$valid){
-			console.log($scope.foto);
-
-			$http.post('/v1/fotos', $scope.foto)
-			.success(function(){
-				$scope.foto = {};
-				$scope.mensagem = 'Foto adicionada com sucesso!';
-			})
-			.error(function(erro){
-				console.log(erro);
-				$scope.mensagem = 'Não foi possível cadastrar a foto';
-			})	
+			
+			if($routeParams.fotoId){
+				$http.put('/v1/fotos/' + $scope.foto._id, $scope.foto)
+				.success(function(){
+					$scope.mensagem = 'Foto ' + $scope.foto.titulo + ' foi alterada com sucesso!';
+				})
+				.error(function(erro){
+					console.log(erro);
+					$scope.mensagem = 'Não foi possível alterar a foto' + $scope.foto.titulo;
+				})
+			}
+			else{
+				$http.post('/v1/fotos', $scope.foto)
+				.success(function(){
+					$scope.foto = {};
+					$scope.mensagem = 'Foto adicionada com sucesso!';
+				})
+				.error(function(erro){
+					console.log(erro);
+					$scope.mensagem = 'Não foi possível cadastrar a foto';
+				})	
+			}
 		}
 	};		
 });
